df6a0da680f29c92a5df7399672cb28445754312
Rafa <jchavarria@soin.co.cr>
2020-08-20 19:41:51 -0600

    test github actions

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const app = express();
 
 // Respond with "Hello World via Github Actions" for requests that hit our root "/"
 app.get("/", function (req, res) {
- return res.status(200).send({message: 'Hello World via Github Actions'})
+ return res.status(200).send({ message: 'Hello World via Github Actions' })
 });
 
 // listen to port 7000 by default
 app.listen(process.env.PORT || 7000, () => {
-  console.log("Server is running");
+  console.log('Server is running');
 });
 
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
-const request = require("supertest");
-const app = require("../index");
+const request = require('supertest');
+const app = require('../index');
 
-describe("GET /", () => {
-  it("respond with Hello World via Github Actions", (done) => {
-    request(app).get("/").expect("Hello World via Github Actions", done);
+describe('GET /', () => {
+  it('respond with Hello World via Github Actions', (done) => {
+    request(app).get('/').expect(200, done);
   })
 });
